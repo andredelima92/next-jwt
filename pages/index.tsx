0e545e6a53fb89f9df759650482c67c800b0c3e9
Andre <andredelima92@gmail.com>
2021-06-03 23:05:38 -0300

    feature: get form datas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,41 @@
+import { useForm } from "react-hook-form";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const { register, handleSubmit } = useForm();
+
+  const handleSignIn = (data) => {
+    console.log(data);
+  };
+
   return (
     <main className={styles.container}>
       <h3>Realize o login</h3>
-      <form>
+      <form onSubmit={handleSubmit(handleSignIn)}>
         <div className="form-group">
-          <input placeholder="E-mail" type="text" className="form-control" />
           <input
+            {...register("email")}
+            name="email"
+            placeholder="E-mail"
+            type="text"
+            className="form-control"
+          />
+          <input
+            {...register("password")}
+            name="password"
             placeholder="Senha"
             type="password"
             className="form-control mt-1"
           />
         </div>
         <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="checkMe" />
+          <input
+            {...register("remember")}
+            name="remember"
+            type="checkbox"
+            className="form-check-input"
+            id="checkMe"
+          />
           <label className="form-check-label" htmlFor="checkMe">
             Lembrar senha
           </label>
